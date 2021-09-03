2a657b1bfb20c114dfd755f87788ac017775ca53
Ernest <ernestduodu@hotmail.co.uk>
2021-09-03 15:11:53 +0100

    Moved modal and overlay to protals

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import Button from './Button';
-import Card from './Card'
-import styles from './Modal.module.css';
+import React from "react";
+import ReactDom from "react-dom";
+import Button from "./Button";
+import Card from "./Card";
+import styles from "./Modal.module.css";
 
+const Backdrop = ({ closeModal }) => (
+  <div className={styles.backdrop} onClick={closeModal}></div>
+);
 
-const Modal = ({title, children, closeModal}) => {
-    return (
-        <>
-            <div className={styles.backdrop } onClick={closeModal}></div>
-            <Card className={styles.modal}>
+const ModalOverlay = ({ title, children, closeModal }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>{children}</div>
+      <footer className={styles.actions}>
+        <Button onClick={closeModal}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
-                <header  className={styles.header}>
-                    <h2>{title}</h2>
-                </header>
-                <div className={styles.content}>
-                    {children}
-                </div>
-                <footer className={styles.actions }>
-                    <Button onClick={closeModal}>Okay</Button>
-                </footer>
-            </Card>
-        </>
-    )
-}
+const Modal = ({ title, children, closeModal }) => {
+  return (
+    <>
+      {ReactDom.createPortal(
+        <Backdrop closeModal={closeModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          closeModal={closeModal}
+          title={title}
+          children={children}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
